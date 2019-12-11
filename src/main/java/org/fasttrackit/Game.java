c0d0625f7a46d0ad58a523fb1486f72759032a47
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2019-12-11 19:23:27 +0200

    studied lists and enhanced for

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,10 +1,14 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
 
     private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<>();
 
 
     public void start() {
@@ -12,36 +16,57 @@ public class Game {
 
         initializeTracks();
         displayTracks();
+        initializeCompetitors();
 
-        int playerCount = getPlayerCountFromUser();
-
-        String vehicleName = getVehicleNameFromUser();
-        System.out.println("Number of players " + playerCount);
-        System.out.println("Name from user: " + vehicleName);
+        // enhanced for
+        for (Vehicle vehicle : competitors) {
+            System.out.println("It`s " + vehicle.getName() + "`s turn.");
+        }
     }
 
-    private void initializeTracks(){
+    private void initializeCompetitors() {
+
+        int playerCount = getPlayerCountFromUser();
+        System.out.println("Number of players " + playerCount);
+
+        for (int i = 1; i <= playerCount; i++) {
+            System.out.println("Adding player " + i);
+
+            Vehicle vehicle = new Vehicle();
+            vehicle.setName(getVehicleNameFromUser());
+            vehicle.setFuelLevel(80);
+            vehicle.setMileage(ThreadLocalRandom.current().nextDouble(6, 15));
+            vehicle.setMaxSpeed(300);
+
+            System.out.println("Vehicle for player " + i + ": " + vehicle.getName() + " - Mileage: " +
+                    vehicle.getMileage());
+
+            competitors.add(vehicle);
+        }
+    }
+
+    private void initializeTracks() {
         Track track1 = new Track();
         track1.setName("Silverstone");
         track1.setLength(4.2);
 
-        tracks[0]=track1;
+        tracks[0] = track1;
 
         Track track2 = new Track();
         track2.setName("Transylvania");
         track2.setLength(400.2);
 
-        tracks[1]=track2;
+        tracks[1] = track2;
     }
 
-    private void displayTracks(){
+    private void displayTracks() {
         System.out.println("Available tracks: ");
 
 //      classic for loop
-        for (int i=0; i<tracks.length; i++){
+        for (int i = 0; i < tracks.length; i++) {
 
-            if (tracks[i]!=null) {
-                System.out.println((i+1)+". "+tracks[i].getName()+" - "+ tracks[i].getLength()+"km");
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + ". " + tracks[i].getName() + " - " + tracks[i].getLength() + "km");
             }
         }
     }
--- src/main/java/org/fasttrackit/JokerVehicle.java
+++ src/main/java/org/fasttrackit/JokerVehicle.java
@@ -5,10 +5,10 @@ public class JokerVehicle extends Vehicle {
     @Override
     public double accelerate(double speed, double durationInHours) {
         System.out.println(getName() + " is accelerating with " + speed +
-                "for "+ durationInHours+ "h");
-        double distance = 2*speed*durationInHours;
+                "for " + durationInHours + "h");
+        double distance = 2 * speed * durationInHours;
 
-        setTravelDistance(getTravelDistance()+distance);
+        setTravelDistance(getTravelDistance() + distance);
         System.out.println("Cheating! ha ha..");
         return distance;
     }
@@ -16,11 +16,11 @@ public class JokerVehicle extends Vehicle {
     // co-variant return type
     //acces cannot be more restrictive than in the parent class
     @Override
-    public JokerVehicle clone(){
+    public JokerVehicle clone() {
         return new JokerVehicle();
     }
 
-    public void fly(){
+    public void fly() {
         System.out.println("Flying!!!");
     }
 }
